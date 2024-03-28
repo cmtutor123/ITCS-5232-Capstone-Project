c7c96a1fcea18031b0f1b08a807dc912c850a7f3
Caleb Tutor <cmtutor123@gmail.com>
2024-03-28 19:50:00 -0400

    Perk Selection UI

---
+++ ITCS-5232-Capstone-Project/Assets/Scripts/UI/PerkButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PerkButton : MonoBehaviour
+{
+    public PerkIcon icon;
+    public TextMeshProUGUI tmpCost;
+
+    public void ChangeColor(Color newColor) => icon.ChangeColor(newColor);
+
+    public void ChangeIcon(Sprite newSprite) => icon.ChangeIcon(newSprite);
+
+    public void ChangeCost(int newCost)
+    {
+        if (newCost > 0)
+        {
+            tmpCost.text = newCost.ToString();
+        }
+        else
+        {
+            tmpCost.text = "";
+        }
+    }
+}

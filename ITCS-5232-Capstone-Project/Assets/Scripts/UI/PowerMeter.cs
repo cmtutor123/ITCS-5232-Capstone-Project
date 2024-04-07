558fba236c7e51b8d9d9a7c653161659e6540c8d
Caleb Tutor <cmtutor123@gmail.com>
2024-04-07 17:40:42 -0400

    Unlock Point display

--- ITCS-5232-Capstone-Project/Assets/Scripts/UI/PowerMeter.cs
+++ ITCS-5232-Capstone-Project/Assets/Scripts/UI/PowerMeter.cs
@@ -6,9 +6,18 @@ using TMPro;
 public class PowerMeter : MonoBehaviour
 {
     public TextMeshProUGUI tmpMeter;
+    public int points = 0;
 
     public void SetMeter(int current, int max)
     {
-        tmpMeter.text = current.ToString() + "/" + max.ToString();
+        string power = current.ToString() + "/" + max.ToString() + " Perk Power";
+        string point = points.ToString() + " Unlock Points\n";
+        if (points == 0) point = "";
+        tmpMeter.text = point + power;
+    }
+
+    public void SetPoints(int points)
+    {
+        this.points = points;
     }
 }

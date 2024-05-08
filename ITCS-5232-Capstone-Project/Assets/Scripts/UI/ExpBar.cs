70dab038249ee2b14a18c9bc71d1fa17082a9200
Caleb Tutor <cmtutor123@gmail.com>
2024-05-08 16:41:31 -0400

    exp text

--- ITCS-5232-Capstone-Project/Assets/Scripts/UI/ExpBar.cs
+++ ITCS-5232-Capstone-Project/Assets/Scripts/UI/ExpBar.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ExpBar : MonoBehaviour
 {
     public ClassEmblem classEmblem;
     public Image expBar;
+    public TextMeshProUGUI expText;
 
     public void SetEmblem(Color colorLight, Color colorDark, int level)
     {
@@ -17,5 +19,6 @@ public class ExpBar : MonoBehaviour
     public void SetExp(int current, int max)
     {
         expBar.fillAmount = Mathf.Clamp(current / max, 0, 1);
+        expText.text = current.ToString() + "/" + max.ToString();
     }
 }
